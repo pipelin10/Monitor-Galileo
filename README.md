7453aab81bddf41606492e4661f2ae3a4759f56c
Felipe Montoya <pipelin1010@Pipe-PC>
2017-04-24 04:07:23 -0500

    Subida script desde Debian

---
+++ README.md
@@ -0,0 +1,15 @@
+# Script para el monitoreo del Galileo
+
+## Integrantes  
+Melissa Millares  
+Felipe Montoya  
+
+## Proposito
+Utilizamos bash para tomar de una board Intel (Galileo) los siguientes datos  
+-Uso de CPU  
+-Uso de RAM  
+-Carga del sistema  
+-Velocidad de descarga  
+-Velocidad de carga  
+-Almacenamiento disponible  
+
---
+++ scriptSO.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+#Uso CPU Usuario
+export usoCpuUsuario=$(top -n1 | grep Cpu | tr -s ' ' | cut -d ' ' -f 2 | tr -s ',' '.')
+echo "Uso cpu usuario: "
+echo $usoCpuUsuario
+
+#Uso CPU Sistema
+export usoCpuSistema=$(top -n1 | grep Cpu | tr -s ' ' | cut -d ' ' -f 4 | tr -s ',' '.')
+echo "Uso cpu sistema: "
+echo $usoCpuSistema
+
+#Uso CPU
+export usoCPU=$(echo $usoCpuSistema + $usoCpuUsuario |bc)
+echo "Uso cpu:"
+echo $usoCPU
+
+#Carga del sistema cada 5 minutos
+export cargaSistema=$(cat /proc/loadavg | cut -d ' ' -f 2)
+echo "Carga del sistema: "
+echo $cargaSistema
+
+
+#Uso de memoria Ram en megabytes
+export usoRAM=$(free -m| grep Mem | tr -s ' ' | cut -d ' ' -f 3)
+echo "Uso memoria RAM:"
+echo $usoRAM
+
+
+#Almacenamiento disponible en megabytes
+export almacenamientoDisp=$(df -m | grep sda1 | tr -s ' ' | cut -d ' ' -f 4)
+echo "Almacenamiento disponible:"
+echo $almacenamientoDisp
+
+#Obtenemos prueba velocidad
+echo -n > pruebaVelocidad
+./speedtest-cli >> pruebaVelocidad
+
+#Velocidad Descarga
+export velDescarga=$(cat pruebaVelocidad | grep Download | cut -d ' ' -f 2)
+echo "Velocidad Descarga:"
+echo $velDescarga
+
+#Velocidad Carga
+export velCarga=$(cat pruebaVelocidad | grep Upload | cut -d ' ' -f 2)
+echo "Velocidad carga:"
+echo $velCarga
+
+#Thing Speak
+curl --silent "http://api.thingspeak.com/update?key=PN4YHO499TVL22D1&field1=$usoCpuUsuario&field2=$usoCpuSistema&field3=$cargaSistema&field4=$usoRAM&field5=$almacenamientoDisp&field6=$velDescarga&field7=$velCarga"
